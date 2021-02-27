79d295c674a2ef9da0d13ac95389fd5c5abfaba2
mertceyhan <c.mertceyhan@hotmail.com>
2021-02-27 22:16:09 +0300

    Initial commit

---
+++ app/src/main/java/com/mertceyhan/contraintlayoutproject/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.mertceyhan.contraintlayoutproject
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.button.MaterialButton
+import com.google.android.material.textfield.TextInputEditText
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val textInputEditTextUsername: TextInputEditText =
+            findViewById(R.id.textInputEditTextUsername)
+
+        val textInputEditTextPassword: TextInputEditText =
+            findViewById(R.id.textInputEditTextPassword)
+
+        val buttonLogin: MaterialButton =
+            findViewById(R.id.buttonLogin)
+
+        buttonLogin.setOnClickListener {
+            Toast.makeText(
+                this,
+                "${textInputEditTextUsername.text} - ${textInputEditTextPassword.text}",
+                Toast.LENGTH_LONG
+            ).show()
+        }
+    }
+}
